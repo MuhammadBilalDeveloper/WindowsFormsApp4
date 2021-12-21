4efa0c172340c94ae2b21b36e715c2f726edc4a3
Muhammad Bilal <yasir007.in@gmail.com>
2021-12-21 17:53:19 +0500

    tt

--- WindowsFormsApp4/Form1.Designer.cs
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -33,9 +33,13 @@ namespace WindowsFormsApp4
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -45,7 +49,7 @@ namespace WindowsFormsApp4
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.Size = new System.Drawing.Size(800, 708);
             this.panel1.TabIndex = 0;
             // 
             // splitContainer1
@@ -57,17 +61,23 @@ namespace WindowsFormsApp4
             // 
             // splitContainer1.Panel1
             // 
-            this.splitContainer1.Panel1.Controls.Add(this.button1);
             this.splitContainer1.Panel1.Controls.Add(this.label1);
-            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
-            this.splitContainer1.SplitterDistance = 188;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.label4);
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 708);
+            this.splitContainer1.SplitterDistance = 313;
             this.splitContainer1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(111, 75);
+            this.label1.Location = new System.Drawing.Point(285, 69);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(161, 31);
             this.label1.TabIndex = 0;
@@ -75,24 +85,58 @@ namespace WindowsFormsApp4
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(437, 61);
+            this.button1.Location = new System.Drawing.Point(291, 272);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(181, 45);
             this.button1.TabIndex = 1;
-            this.button1.Text = "OK";
+            this.button1.Text = "Update";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(198, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(426, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = " Release 1.0.2 is Now Available";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(198, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(426, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = " Release 1.0.3 is Now Available";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(198, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(426, 31);
+            this.label4.TabIndex = 3;
+            this.label4.Text = " Release 1.0.4 is Now Available";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 708);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -105,6 +149,9 @@ namespace WindowsFormsApp4
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- WindowsFormsApp4/Form1.cs
+++ WindowsFormsApp4/Form1.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Squirrel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,35 @@ namespace WindowsFormsApp4
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                await UpdateApplication();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "WindowsFormsApp", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private async Task UpdateApplication()
+        {
+            try
+            {
+                ServicePointManager.Expect100Continue = true;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                using (var mgr = await UpdateManager.GitHubUpdateManager("https://github.com/MuhammadBilalDeveloper/WindowsFormsApp4"))
+                {
+                    await mgr.UpdateApp();
+                }
+                MessageBox.Show("Release is updated.", "WindowsFormsApp", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "WindowsFormsApp", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
